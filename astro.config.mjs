b268cdb1d6a57c2665a2c06c53ed67887ec4b964
Juan Manuel Tejeda Torres <40574209+noobcooder007@users.noreply.github.com>
2023-12-29 23:28:42 -0600

    astro build again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://noobcooder007.github.io',
+  base: '/portfolio.dev',
+  trailingSlash: 'always',
   outDir: "./docs",
 });
