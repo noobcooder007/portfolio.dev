e77ddebf358de7080bb531eb1b3841d92cefdf93
Juan Manuel Tejeda Torres <40574209+noobcooder007@users.noreply.github.com>
2023-12-29 23:22:19 -0600

    default configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://noobcooder007.github.io',
-  base: '/portfolio.dev/docs',
   outDir: "./docs",
 });
